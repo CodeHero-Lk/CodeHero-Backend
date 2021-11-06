68537c71fa16a456101ac89a45fd366a9a75b7df
Sihilel Himasara <sihilelh@gmail.com>
2021-11-06 12:58:52 +0530

    Server env update & Create Dev Server

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const http = require('http');
 const app = require('./app');
 
-const PORT = process.env.PORT || 8000;
 
-const server = app.listen(PORT, () => {
-    console.log(`App started on PORT:${PORT}`);
-});
+// Get port from environment and run the server.
+let server = undefined;
+if (process.env.NODE_ENV === 'development') {
+    const DEV_PORT = process.env.APP_DEV_PORT || 3000;
+    server = http.createServer(app).listen(DEV_PORT, () => {
+        console.log(`Development server running on port ${DEV_PORT}`);
+    });
+} else {
+    const PORT = process.env.APP_PORT || 80;
+    server = http.createServer(app).listen(PORT, () => {
+        console.log(`Development server running on port ${PORT}`);
+    });
+}
 
 module.exports = server;
